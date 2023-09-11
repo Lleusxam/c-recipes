7aaae1e6f1e3182f1ab33764cff3c2617feb823e
lleusxam <blkleusxam@hotmail.com>
2023-09-11 11:19:22 -0300

    v0.4 Add: Adição do sistema de navegação entre os menus

--- main.c
+++ main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
-void menu_principal() {
+    char menu_principal() {
+    char opt;
     printf("=============================\n");
     printf("        MENU PRINCIPAL       \n");
     printf("=============================\n");
     printf("|   1. Menu de Receitas     |\n");
     printf("|   2. Menu de Autores      |\n");
     printf("|   3. Menu de Cardapio     |\n");
+    printf("|   0. Finalizar o programa |\n");
     printf("=============================\n");
+    printf("\n");
+    printf("Escolha sua opcao: ");
+    scanf("%c", &opt);
+    getchar();
+    printf("\n");
+    printf("Pressione ENTER para continuar...");
+    getchar();
+    return opt;
 }
 
-void menu_receitas() {
+char menu_receitas() {
     char opt;
     printf("=============================\n");
     printf("        MENU RECEITAS        \n");
@@ -20,6 +30,7 @@ void menu_receitas() {
     printf("|   3. Deletar receita      |\n");
     printf("|   4. Atualizar receita    |\n");
     printf("|   5. Pesquisar receita    |\n");
+    printf("|   0. Retornar             |\n");
     printf("=============================\n");
     printf("\n");
     printf("Escolha sua opcao: ");
@@ -28,9 +39,10 @@ void menu_receitas() {
     printf("\n");
     printf("Pressione ENTER para continuar...");
     getchar();
+    return opt;
 }
 
-void menu_autores() {
+char menu_autores() {
     char opt;
     printf("=============================\n");
     printf("        MENU AUTORES         \n");
@@ -40,6 +52,7 @@ void menu_autores() {
     printf("|   3. Deletar autor        |\n");
     printf("|   4. Atualizar autor      |\n");
     printf("|   5. Pesquisar autor      |\n");
+    printf("|   0. Retornar             |\n");
     printf("=============================\n");
     printf("\n");
     printf("Escolha sua opcao: ");
@@ -48,9 +61,10 @@ void menu_autores() {
     printf("\n");
     printf("Pressione ENTER para continuar...");
     getchar();
+    return opt;
 }
 
-void menu_cardapio() {
+char menu_cardapio() {
     char opt;
     printf("=============================\n");
     printf("        MENU CARDAPIO         \n");
@@ -60,6 +74,7 @@ void menu_cardapio() {
     printf("|   3. Deletar cardapio      |\n");
     printf("|   4. Atualizar cardapio    |\n");
     printf("|   5. Pesquisar cardapio    |\n");
+    printf("|   0. Retornar             |\n");
     printf("=============================\n");
     printf("\n");
     printf("Escolha sua opcao: ");
@@ -68,6 +83,7 @@ void menu_cardapio() {
     printf("\n");
     printf("Pressione ENTER para continuar...");
     getchar();
+    return opt;
 }
 
 void sobre() {
@@ -78,21 +94,96 @@ void sobre() {
 }
 
 int main(){
+    char opt, opt1;
     sobre();
     
-    printf("\n");
+    printf("\nPressione ENTER para continuar...");
+    getchar();
 
-    menu_principal();
-    
-    printf("\n");
-    
-    menu_receitas();
-    
-    printf("\n");
-    
-    menu_autores();
-    
-    printf("\n");
-    
-    menu_cardapio();
+    do {
+        opt = menu_principal();
+        switch(opt) {
+            case '1':
+                opt1 = menu_receitas();
+                switch(opt1) {
+                    case '1':
+                        printf("Cadastrar receita\n");
+                        break;
+                    case '2':
+                        printf("Editar receita\n");
+                        break;
+                    case '3':
+                        printf("Deletar receita\n");
+                        break;
+                    case '4':
+                        printf("Atualizar receita\n");
+                        break;
+                    case '5':
+                        printf("Pesquisar receita\n");
+                        break;
+                    case '0':
+                        printf("Voltando ao menu principal...\n");
+                        break;
+                    default:
+                        printf("Opcao invalida");
+                }
+                break;
+            case '2':
+                opt1 = menu_autores();
+                switch(opt1) {
+                    case '1':
+                        printf("Cadastrar autor\n");
+                        break;
+                    case '2':
+                        printf("Editar autor\n");
+                        break;
+                    case '3':
+                        printf("Deletar autor\n");
+                        break;
+                    case '4':
+                        printf("Atualizar autor\n");
+                        break;
+                    case '5':
+                        printf("Pesquisar autor\n");
+                        break;
+                    case '0':
+                        printf("Voltando ao menu principal...\n");
+                        break;
+                    default:
+                        printf("Opcao invalida");
+                }
+                break;
+            case '3':
+                opt1 = menu_cardapio();
+                switch(opt1) {
+                    case '1':
+                        printf("Cadastrar cardapio\n");
+                        break;
+                    case '2':
+                        printf("Editar cardapio\n");
+                        break;
+                    case '3':
+                        printf("Deletar cardapio\n");
+                        break;
+                    case '4':
+                        printf("Atualizar cardapio\n");
+                        break;
+                    case '5':
+                        printf("Pesquisar cardapio\n");
+                        break;
+                    case '0':
+                        printf("Voltando ao menu principal...\n");
+                        break;
+                    default:
+                        printf("Opcao invalida");
+                }
+            case '0':
+                printf("Finalizando o programa...\n");
+                break;
+            default:
+                printf("Opcao invalida");
+        }
+    } while(opt != '0');
+
+    return 0;
 }
